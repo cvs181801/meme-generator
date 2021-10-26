3b3cea9fad27ba38b590ee13be9176501805dc82
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-26 10:03:27 -0700

    add fetch to fetch resource

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import React from "react"
+import Memegenerator from "./components/Memegenerator"
 
 function App() {
   return (
     <div className="App">
    <p>hi</p>
+   <Memegenerator/>
     </div>
   );
 }
--- src/components/Memegenerator.js
+++ src/components/Memegenerator.js
@@ -4,14 +4,30 @@ class Memegenerator extends React.Component {
     constructor() {
         super()
         this.state = {
-            meme: ""
+            topText: "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
     }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data => {
+                //const [memes] = data
+                console.log(data.data.memes[0])
+                // this.setState({allMemeImgs: [memes]})
+                 })
+    }
+
     render(){
         return (
             <div>
-                <p>add resource here</p>
+                <p src={this.state.randomImage}>Random Image</p>
             </div>
         )
     }
 }
+
+export default Memegenerator
