9b4bef4cfd9f63b783917ed96d0e50ca99d48787
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-26 09:37:25 -0700

    add basic component set up for memegenerator

---
+++ src/components/Memegenerator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Memegenerator extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            meme: ""
+        }
+    }
+    render(){
+        return (
+            <div>
+                <p>add resource here</p>
+            </div>
+        )
+    }
+}
